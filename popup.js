be89e1342f3419e8a229ef9c947da291b4354156
Robin J <robin__j@outlook.com>
2021-08-16 12:34:20 +0530

    entering the main loop after threshold is reached.MinMembers set to 20

--- popup.js
+++ popup.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 async function attendPolls() {
     var attended = [];
     const DELAY = 2000; //2 seconds
-    const MIN_MEMBERS = 15;
+    const MIN_MEMBERS = 20;
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -63,30 +63,6 @@ async function attendPolls() {
         }
     }
 
-    //auto exit impleimentation (TODO)
-
-    function checkTime() {
-        var d = new Date();
-        var currtime = d.getHours() * 100 + d.getMinutes();
-        // now your currtime looks like 530 if it's 5.30am, or 1730 if it's 5.30 pm
-        // you can just do a simple comparison between ints
-
-        console.log("time", currtime);
-
-        if ((currtime > 9040 && currtime < 9050) ||
-            (currtime > 1040 && currtime < 1050) ||
-            (currtime > 1150 && currtime < 1210) ||
-            (currtime > 1250 && currtime < 1310) ||
-            (currtime > 1450 && currtime < 1510)) {
-            // closed between 20:00 (8 pm) and 8:00 (8 am) as an example
-
-            // console.log("true");
-            return true;
-        }
-        // console.log("false");
-        return false;
-    }
-
     function getCount() {
         const peopleCount = document.getElementsByClassName("uGOf1d");
         var count = parseInt(peopleCount[0]["innerText"]);
@@ -104,6 +80,15 @@ async function attendPolls() {
     await sleep(1000);
     togglePoll();
     await sleep(1000);
+
+    //Enter the other loop only after the threshold is reached
+    while (true) {
+        let count = getCount();
+        await sleep(DELAY);
+        if (count > (MIN_MEMBERS + 5)) {
+            break;
+        }
+    }
 
 
     while (true) {
